f3ada3457402d6cf3805c6f90607e305fc53cca8
o77777o <136020078+o77777o@users.noreply.github.com>
2023-07-28 21:14:38 +0300

    Update 2script.js

--- 2script.js
+++ 2script.js
@@ -1,16 +1,7 @@
 "use strict";
 
-alert ('Hello world!') ;
+let age = prompt('Сколько тебе лет?', 100);
+alert(`Тебе ${age} лет!`); // Тебе 100 лет!
 
-let message = 'hello'
-alert (message)
-
-let admin
-let name = 'John' ;
-
-admin = name
-
-alert (admin)
-alert (name)
-
-prompt
+let isBoss = confirm("Ты здесь главный?");
+alert( isBoss ); // true, если нажата OK
