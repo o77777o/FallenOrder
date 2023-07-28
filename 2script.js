8a486d89794096672c62271114041574364a17f4
o77777o <136020078+o77777o@users.noreply.github.com>
2023-07-28 18:01:56 +0300

    Update 2script.js

--- 2script.js
+++ 2script.js
@@ -1,2 +1,16 @@
+"use strict";
+
 alert ('Hello world!') ;
-alert ('Hello world!') ; 
+
+let message = 'hello'
+alert (message)
+
+let admin
+let name = 'John' ;
+
+admin = name
+
+alert (admin)
+alert (name)
+
+prompt
