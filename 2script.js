225a09ef7c641338349b7711cde4ca4c6419f80f
o77777o <136020078+o77777o@users.noreply.github.com>
2023-08-02 23:21:19 +0300

    Update 2script.js

--- 2script.js
+++ 2script.js
@@ -1,6 +1,20 @@
-let NameOfJavaScript = prompt(`Какое другое название у JavaScript?`)
-if (NameOfJavaScript == `ECMAScript`) {
-    alert(`Верно!`)
+let value = prompt(`Введите только число`)
+if (value > 0) {
+  alert(1)
+} else if (value < 0) {
+  alert(-1)
 } else {
-  alert(`Не знаете? ECMAScript`)
+  alert(0)
 }
+
+
+
+
+
+
+// let NameOfJavaScript = prompt(`Какое другое название у JavaScript?`)
+// if (NameOfJavaScript == `ECMAScript`) {
+//     alert(`Верно!`)
+// } else {
+//   alert(`Не знаете? ECMAScript`)
+// }
