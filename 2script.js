0ee80a275243b1c755b78d1974b1025315f34de5
o77777o <136020078+o77777o@users.noreply.github.com>
2023-08-02 22:46:15 +0300

    Update 2script.js

--- 2script.js
+++ 2script.js
@@ -1,7 +1,8 @@
-"use strict";
+let age = prompt('Возраст?', 18);
 
-let age = prompt('Сколько тебе лет?', 100);
-alert(`Тебе ${age} лет!`); // Тебе 100 лет!
+let message = (age < 3) ? 'Здравствуй, малыш!' :
+  (age < 18) ? 'Привет!' :
+  (age < 100) ? 'Здравствуйте!' :
+  'Какой необычный возраст!';
 
-let isBoss = confirm("Ты здесь главный?");
-alert( isBoss ); // true, если нажата OK
+alert( message );
