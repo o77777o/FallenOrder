19d15fb89038a82034e136ef19614af2e5c7526c
o77777o <136020078+o77777o@users.noreply.github.com>
2023-08-03 23:51:02 +0300

    Update 2script.js

--- 2script.js
+++ 2script.js
@@ -1,15 +1,52 @@
-let value = prompt(`Введите только число`)
-if (value > 0) {
-  alert(1)
-} else if (value < 0) {
-  alert(-1)
+let login = prompt(`Кто ты?`) ;
+let message = (login=="Сотрудник") ? `Привет` : ?
+
+
+
+if (login == 'Сотрудник') {
+  message = 'Привет';
+} else if (login == 'Директор') {
+  message = 'Здравствуйте';
+} else if (login == '') {
+  message = 'Нет логина';
 } else {
-  alert(0)
+  message = '';
 }
 
 
+alert(message)
 
 
+// let a = prompt (`число А?`) ;
+// let b = prompt (`число В?`) ;
+// let condition = (+a + +b < 4) ? 'Мало' : 'Много' ;
+// alert (condition)
+
+// let yap = условие ? значение1 : значение2;
+// let age = prompt(`сколько лет?`)
+// let accessAllowed = (age > 18) ? true : false;
+// alert (accessAllowed)
+//Сначала вычисляется условие: если оно истинно, тогда возвращается значение1, в противном случае – значение2.
+
+// let accessAllowed;
+// let age = prompt('Сколько вам лет?', '');
+
+// if (age > 18) {
+//   accessAllowed = true;
+// } else {
+//   accessAllowed = false;
+// }
+
+// alert(accessAllowed);
+
+// let value = prompt(`Введите только число`)
+// if (value > 0) {
+//   alert(1)
+// } else if (value < 0) {
+//   alert(-1)
+// } else {
+//   alert(0)
+// }
 
 
 // let NameOfJavaScript = prompt(`Какое другое название у JavaScript?`)
