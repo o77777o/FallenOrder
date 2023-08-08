1b613dcafb472f775aa1c13c30aea56df1a0761e
o77777o <136020078+o77777o@users.noreply.github.com>
2023-08-08 22:24:40 +0300

    Update 2script.js

--- 2script.js
+++ 2script.js
@@ -1,24 +1,68 @@
+let login = prompt("Введите логин")
+let CorrectUserName = "Админ"
+
+if (login==CorrectUserName) {
+    Password
+} else if (login==null) {
+    alert("Отменено")
+} else {
+    alert("Я вас не знаю")
+}
+
+alert (login)
+
+
+
+
+
+let Password = prompt("Введите пароль")
+let CorrectUserPassword = "Я главный"
+
+
+
+
+
+
+
+
+// (CurrentUserName==null) ? "Отменено" : "Я вас не знаю" ;
+
+// let UserPassword = prompt("Введите пароль")
+// let AccsesDone =(UserPassword=="Я главный") ? "Здравствуйте!" : "Неверный пароль"
+
+// if (Accses=="Отменено" || Accses=="Я вас не знаю") {
+//     alert (Accses)
+// }
+
+
+
+
+
+
+
+
+
+// let hour = 12;
+// let isWeekend = true;
+
+// if (hour < 10 || hour > 18 || isWeekend) {
+//   alert( 'Офис закрыт.' ); // это выходной
+// }
+
 // let age = prompt ("сколько лет?") ;
 // let count = (14 <= +age && +age <= 90) ? "Welcome" : "Good bye" ;
 
 // alert (count)
 
-let age = prompt ("сколько лет?") ;
-let count = (14 >= +age && 90 >= +age) ? "Hello" : "Goodbye" ;
-
-alert (count)
-
 // let age = prompt ("сколько лет?") ;
-// let count = (!(14 <= +age && +age <= 90)) ? "Good Bye" : "Hello" ;
+// let count = (14 > +age || +age > 90) ? "Goodbye" : "Hello" ;
 
 // alert (count)
 
+// // let age = prompt ("сколько лет?") ;
+// // let count = (!(14 <= +age && +age <= 90)) ? "Good Bye" : "Hello" ;
 
-
-
-
-
-
+// // alert (count)
 
 // let login = prompt(`Кто ты?`) ;
 // let message = (login== "Сотрудник") ? `Привет` :
