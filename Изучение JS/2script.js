8a0d7bbe0fca384a194390b6a78f5c0d81d47234
o77777o <136020078+o77777o@users.noreply.github.com>
2023-10-01 15:26:53 +0300

    Update 2script.js

--- Изучение JS/2script.js
+++ Изучение JS/2script.js
@@ -1,112 +1,11 @@
-let user = {
-  age: 30,
-};
+let a = "my-short-string";
 
-let key = "age";
-alert(key in user); // true, имя свойства было взято из переменной key
+b = a.split("");
 
-alert(user.key)
+console.log(b);
 
-// let user = {
-//   name: "Babayka",
-//   age: 30,
-//   hobbie: "Pugat ludey",
-// };
+c = b.map(i => {
+  i.length
+});
 
-// let fer = prompt("Кашу ел?", "name" )
-
-// alert( user[fer] );
-
-// let whoAreYou = prompt("Кто ты?");
-
-// if (whoAreYou === null) {
-//     alert("Отменено");
-// } else if (whoAreYou === "Админ") {
-//     let userPassword = prompt("Введите пароль");
-
-//     if (userPassword === null) {
-//         alert("Отменено");
-//     } else if (userPassword === "Я главный") {
-//         alert("Здравствуйте!");
-//     } else {
-//         alert("Неверный пароль");
-//     }
-// } else {
-//     alert("Я вас не знаю");
-// }
-
-// (CurrentUserName==null) ? "Отменено" : "Я вас не знаю" ;
-
-// let UserPassword = prompt("Введите пароль")
-// let AccsesDone =(UserPassword=="Я главный") ? "Здравствуйте!" : "Неверный пароль"
-
-// if (Accses=="Отменено" || Accses=="Я вас не знаю") {
-//     alert (Accses)
-// }
-
-// let hour = 12;
-// let isWeekend = true;
-
-// if (hour < 10 || hour > 18 || isWeekend) {
-//   alert( 'Офис закрыт.' ); // это выходной
-// }
-
-// let age = prompt ("сколько лет?") ;
-// let count = (14 <= +age && +age <= 90) ? "Welcome" : "Good bye" ;
-
-// alert (count)
-
-// let age = prompt ("сколько лет?") ;
-// let count = (14 > +age || +age > 90) ? "Goodbye" : "Hello" ;
-
-// alert (count)
-
-// // let age = prompt ("сколько лет?") ;
-// // let count = (!(14 <= +age && +age <= 90)) ? "Good Bye" : "Hello" ;
-
-// // alert (count)
-
-// let login = prompt(`Кто ты?`) ;
-// let message = (login== "Сотрудник") ? `Привет` :
-// (login== "Директор") ? "Здравствуйте" :
-// (login== "") ? "Нет логина" : "Введите корректное значение" ;
-
-// alert(message)
-
-// let a = prompt (`число А?`) ;
-// let b = prompt (`число В?`) ;
-// let condition = (+a + +b < 4) ? 'Мало' : 'Много' ;
-// alert (condition)
-
-// let yap = условие ? значение1 : значение2;
-// let age = prompt(`сколько лет?`)
-// let accessAllowed = (age > 18) ? true : false;
-// alert (accessAllowed)
-//Сначала вычисляется условие: если оно истинно, тогда возвращается значение1, в противном случае – значение2.
-
-// let accessAllowed;
-// let age = prompt('Сколько вам лет?', '');
-
-// if (age > 18) {
-//   accessAllowed = true;
-// } else {
-//   accessAllowed = false;
-// }
-
-// alert(accessAllowed);
-
-// let value = prompt(`Введите только число`)
-// if (value > 0) {
-//   alert(1)
-// } else if (value < 0) {
-//   alert(-1)
-// } else {
-//   alert(0)
-// }
-
-// let NameOfJavaScript = prompt(`Какое другое название у JavaScript?`)
-// if (NameOfJavaScript == `ECMAScript`) {
-//     alert(`Верно!`)
-// } else {
-//   alert(`Не знаете? ECMAScript`)
-// }
+console.log(c);
