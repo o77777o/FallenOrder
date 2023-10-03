2f39c2422970c7a3c438359bdbbc8eec29987f32
o77777o <136020078+o77777o@users.noreply.github.com>
2023-10-04 02:04:59 +0300

    и

--- Изучение JS/2script.js
+++ Изучение JS/2script.js
@@ -1,11 +1,16 @@
-let a = "my-short-string";
+// Получаем ссылку на кнопку на вашем веб-сайте
+const websiteButton = document.querySelector('.website-button');
 
-b = a.split("");
+// Функция, которая будет вызвана при нажатии кнопки на клавиатуре
+function handleKeyPress(event) {
+  // Проверяем, является ли нажатая клавиша той, которую вы хотите привязать к кнопке
+  if (event.key === 'Enter') {
+    // Симулируем клик на кнопку на вашем веб-сайте
+    websiteButton.click();
+    a += 1
+    console.log("a")
+  }
+}
 
-console.log(b);
-
-c = b.map(i => {
-  i.length
-});
-
-console.log(c);
+// Отслеживаем событие нажатия кнопки на клавиатуре
+document.addEventListener('keydown', handleKeyPress);
